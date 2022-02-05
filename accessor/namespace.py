f0090ae7488032972c8596f71e58d1a0e2104c89
Carl Corneil <aecorn@gmail.com>
2022-02-05 21:29:57 +0100

    Fix comments accessors

--- accessor/namespace.py
+++ accessor/namespace.py
@@ -1,24 +1,25 @@
 import pandas as pd
 
 # Shared functionality under the namespace in 
-class AvailableBoth:
+class DfSMixin:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
     def title_print(self):
         print(self._obj.attrs['title'])
 
-
+# Accessor under series
 @pd.api.extensions.register_series_accessor("ext")
-class ExtSeriesAccessor(AvailableBoth):
+class ExtSeriesAccessor(DfSMixin):
     pass
     # Title_set() here might be confusing, because you would set the name of the column?
+    # Attrs is also weird, documentation says it should be global on Series for the dataset
+    # But testing shows that the Series has its own attrs?
 
 
-# Accessor under dataframes, inherit from Series is possible?
-# To avoid 
+# Accessor under dataframes, inherits from mixin 
 @pd.api.extensions.register_dataframe_accessor("ext")
-class ExtAccessor(AvailableBoth):
+class ExtAccessor(DfSMixin):
 
     def title_set(self, title):
         self._obj.attrs['title'] = title
