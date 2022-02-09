2e7f5a9d2bf1a31bc72fecbfa5d6b18348e2992f
Carl Corneil <aecorn@gmail.com>
2022-02-09 10:55:45 +0100

    Loose function also in subclass

--- subclass/subclass.py
+++ subclass/subclass.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
+# "Loose" function
+def set_title(df: pd.DataFrame, title: str) -> pd.DataFrame:
+    if isinstance(df, pd.DataFrame) or issubclass(type(df), pd.DataFrame):
+        df.attrs['title'] = title
+        return
+    raise KeyError(f"df: {df.name} doesnt look like a Dataframe, I'm scared of adding title.")
+
+
 class DfSubclass(pd.DataFrame):
     
     def title_set(self, title):
-        self.attrs['title'] = title
+        # "Registering" a loose function under a subclass?
+        self = set_title(self, title)
 
     def title_print(self):
         print(self.attrs['title'])
--- subclass/try_subclass.py
+++ subclass/try_subclass.py
@@ -1,8 +1,8 @@
-import subclass
+import subclass as subc
 
 def main():
     # initialize like a pd.DataFrame
-    tab = subclass.DfSubclass({
+    tab = subc.DfSubclass({
         'age' : ["9-15", "16-24", "25-44"],
         'year' : ["2021", "2021", "2021"],
         'amount' : ["10,5", "15,1", "22,0"]
@@ -10,6 +10,10 @@ def main():
 
     # New methods
     tab.title_set("05300")
+    tab.title_print()
+
+    # Also available as loose function, confusing?
+    subc.set_title(tab, "05302")
     tab.title_print()
 
     print(type(tab))
